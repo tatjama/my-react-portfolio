dbc5bd775e9d1939e7723a0d3d533c6e582c201e
Tanja <tanja120a@gmail.com>
2020-12-06 12:06:22 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
+//Pages import
+import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
+import ContactUs from './pages/ContactUs';
+import Nav from './components/Nav';
+//Router
+import { Switch, Route, useLocation } from 'react-router-dom';
+//Animations
+import {AnimatePresence} from 'framer-motion';
+
+function App() {
+  const location = useLocation();
+  console.log(location)
+  return (
+    <div className="App">
+      <GlobalStyle/>
+      <Nav/>  
+      <AnimatePresence exitBeforeEnter>
+        <Switch location = {location} key = {location.pathname}>
+          <Route path = "/" exact>
+              <AboutUs/> 
+          </Route>
+          <Route path = "/work" exact>
+            <OurWork/> 
+          </Route>
+          <Route path = "/work/:id">
+            <MovieDetail/>
+          </Route>
+          <Route path = "/contact">
+            <ContactUs/>
+          </Route>         
+        </Switch>    
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default App;
